13176a500fc00c5f87f75e3d36dfd6231c9fa95f
CodeingBoy <CodeingBoy@gmail.com>
2018-06-13 16:34:35 +0800

    Add test case and file

---
+++ src/test/java/me/codeingboy/litespring/BeanFactoryTest.java
@@ -0,0 +1,34 @@
+package me.codeingboy.litespring;
+
+import me.codeingboy.litespring.beans.BeanDefinition;
+import me.codeingboy.litespring.beans.factory.BeanFactory;
+import me.codeingboy.litespring.beans.factory.support.DefaultBeanFactory;
+import me.codeingboy.litespring.services.PetStoreService;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Test for {@link BeanFactory}
+ *
+ * @author CodeingBoy
+ * @version 1
+ */
+public class BeanFactoryTest {
+    private final static String BEAN_ID_PET_STORE_SERVICE = "petStoreService";
+
+    @Test
+    public void beanGetTest() {
+        BeanFactory factory = new DefaultBeanFactory("petstore-v1.xml");
+        BeanDefinition beanDefinition = factory.getBeanDefinition(BEAN_ID_PET_STORE_SERVICE);
+
+        assertEquals("me.codeingboy.litespring.services.PetStoreService", beanDefinition.getClassName());
+
+        Object bean = factory.getBean(BEAN_ID_PET_STORE_SERVICE);
+        assertNotNull(bean);
+        assertTrue(bean instanceof PetStoreService);
+
+        PetStoreService petStoreService = (PetStoreService) bean;
+    }
+}
