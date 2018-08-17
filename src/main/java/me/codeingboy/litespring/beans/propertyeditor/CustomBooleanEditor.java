1d0dd5ac328d42d9172e6ce8fbf1591a52611016
CodeingBoy <CodeingBoy@gmail.com>
2018-08-17 12:59:57 +0800

    Add CustomBooleanEditor implementation

---
+++ src/main/java/me/codeingboy/litespring/beans/propertyeditor/CustomBooleanEditor.java
@@ -0,0 +1,43 @@
+package me.codeingboy.litespring.beans.propertyeditor;
+
+import me.codeingboy.litespring.utils.StringUtils;
+
+import java.beans.PropertyEditorSupport;
+import java.util.Arrays;
+import java.util.Collection;
+
+/**
+ * A property editor for {@link Boolean}
+ *
+ * @author CodeingBoy
+ * @version 1
+ * @see PropertyEditorSupport
+ */
+public class CustomBooleanEditor extends PropertyEditorSupport {
+    private final static Collection<String> trueStrings = Arrays.asList("true", "yes", "1");
+    private final static Collection<String> falseStrings = Arrays.asList("false", "no", "0");
+    private boolean allowEmpty;
+
+    public CustomBooleanEditor(boolean allowEmpty) {
+        this.allowEmpty = allowEmpty;
+    }
+
+    public CustomBooleanEditor() {
+        this(true);
+    }
+
+    @Override
+    public void setAsText(String text) throws IllegalArgumentException {
+        if (allowEmpty && !StringUtils.hasLength(text)) {
+            setValue(null);
+        }
+
+        if (trueStrings.contains(text)) {
+            setValue(true);
+        } else if (falseStrings.contains(text)) {
+            setValue(false);
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
+}
---
+++ src/test/java/me/codeingboy/litespring/CustomBooleanEditorTest.java
@@ -0,0 +1,99 @@
+package me.codeingboy.litespring;
+
+import me.codeingboy.litespring.beans.propertyeditor.CustomBooleanEditor;
+import me.codeingboy.litespring.beans.propertyeditor.CustomNumberEditor;
+import org.junit.Test;
+
+import static junit.framework.TestCase.*;
+
+/**
+ * Test of {@link CustomBooleanEditor}
+ *
+ * @author CodeingBoy
+ * @version 1
+ * @see CustomBooleanEditor
+ */
+public class CustomBooleanEditorTest {
+
+    @Test
+    public void trueStringTest() {
+        CustomBooleanEditor editor = new CustomBooleanEditor(true);
+
+        editor.setAsText("true");
+        Object value = editor.getValue();
+        assertNotNull(value);
+        assertTrue(value instanceof Boolean);
+        assertTrue((Boolean) value);
+    }
+
+    @Test
+    public void falseStringTest() {
+        CustomBooleanEditor editor = new CustomBooleanEditor(true);
+
+        editor.setAsText("false");
+        Object value = editor.getValue();
+        assertNotNull(value);
+        assertTrue(value instanceof Boolean);
+        assertFalse((Boolean) value);
+    }
+
+    @Test
+    public void yesStringTest() {
+        CustomBooleanEditor editor = new CustomBooleanEditor(true);
+
+        editor.setAsText("yes");
+        Object value = editor.getValue();
+        assertNotNull(value);
+        assertTrue(value instanceof Boolean);
+        assertTrue((Boolean) value);
+    }
+
+    @Test
+    public void noStringTest() {
+        CustomBooleanEditor editor = new CustomBooleanEditor(true);
+
+        editor.setAsText("no");
+        Object value = editor.getValue();
+        assertNotNull(value);
+        assertTrue(value instanceof Boolean);
+        assertFalse((Boolean) value);
+    }
+
+    @Test
+    public void oneStringTest() {
+        CustomBooleanEditor editor = new CustomBooleanEditor(true);
+
+        editor.setAsText("1");
+        Object value = editor.getValue();
+        assertNotNull(value);
+        assertTrue(value instanceof Boolean);
+        assertTrue((Boolean) value);
+    }
+
+    @Test
+    public void zeroStringTest() {
+        CustomBooleanEditor editor = new CustomBooleanEditor(true);
+
+        editor.setAsText("0");
+        Object value = editor.getValue();
+        assertNotNull(value);
+        assertTrue(value instanceof Boolean);
+        assertFalse((Boolean) value);
+    }
+
+    @Test
+    public void emptyStringTest() {
+        CustomNumberEditor editor = new CustomNumberEditor(Integer.class, true);
+
+        editor.setAsText("");
+        Object value = editor.getValue();
+        assertNull(value);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void invalidStringTest() {
+        CustomNumberEditor editor = new CustomNumberEditor(Integer.class, true);
+
+        editor.setAsText("3.1");
+    }
+}
